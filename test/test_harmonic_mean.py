534425ac2be8c6306be7987d2fbf627210bd3a23
Bill Melvin <bill@billmelvin.com>
2023-06-02 16:33:23 -0400

    Add black formatter. Apply reformat

--- test/test_harmonic_mean.py
+++ test/test_harmonic_mean.py
@@ -7,19 +7,11 @@ from imppkg.harmony import main
 
 
 @pytest.mark.parametrize(
-    "inputs, expected_value",
-    [
-        (["1", "4", "4"], 2.0),
-        ([], 0.0),
-        (["badinput"], 0.0)
-    ]
+    "inputs, expected_value", [(["1", "4", "4"], 2.0), ([], 0.0), (["badinput"], 0.0)]
 )
 def test_harmony_inputs(inputs, expected_value, monkeypatch, capsys):
     monkeypatch.setattr(sys, "argv", ["harmony"] + inputs)
     main()
     assert capsys.readouterr().out.strip() == colored(
-        expected_value,
-        "red",
-        "on_yellow",
-        attrs = ["bold"]
+        expected_value, "red", "on_yellow", attrs=["bold"]
     )
