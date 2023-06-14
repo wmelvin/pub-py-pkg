8ca457faadd84d0247aa565d4d1e958fc15e5712
Bill Melvin <bill@billmelvin.com>
2023-06-13 22:39:04 -0400

    Configure readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+
 from importlib import metadata
 
 PACKAGE_VERSION = metadata.version("pubpypack-harmony-bill-melvin")
@@ -34,3 +36,27 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+if os.environ.get("READTHEDOCS") == "True":
+    from pathlib import Path
+
+    PROJECT_ROOT = Path(__file__).parent.parent
+    PACKAGE_ROOT = PROJECT_ROOT / "src" / "imppkg"
+
+    def run_apidoc(_):
+        from sphinx.ext import apidoc
+        apidoc.main([
+            "--force",
+            "--implicit-namespaces",
+            "--module-first",
+            "--separate",
+            "-o",
+            str(PROJECT_ROOT / "docs" / "reference"),
+            str(PACKAGE_ROOT),
+            str(PACKAGE_ROOT / "*.c"),
+            str(PACKAGE_ROOT / "*.so"),
+        ])
+
+    def setup(app):
+        app.connect("builder-inited, run_apidoc")
+
