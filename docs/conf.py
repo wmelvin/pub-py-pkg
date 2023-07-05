6d560be8c42769f886cb5e97956b39b8c77b6324
Bill Melvin <bill@billmelvin.com>
2023-07-05 16:44:28 -0400

    Add pre-commit hooks for black and flake8

--- docs/conf.py
+++ docs/conf.py
@@ -15,9 +15,9 @@ PACKAGE_VERSION = metadata.version("pubpypack-harmony-bill-melvin")
 version = PACKAGE_VERSION
 release = PACKAGE_VERSION
 
-project = 'pubpypack-harmony-bill-melvin'
-copyright = '2023, Bill Melvin'
-author = 'Bill Melvin'
+project = "pubpypack-harmony-bill-melvin"
+copyright = "2023, Bill Melvin"
+author = "Bill Melvin"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -27,16 +27,15 @@ extensions = [
     "sphinx.ext.autodoc.typehints",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 if os.environ.get("READTHEDOCS") == "True":
     from pathlib import Path
@@ -46,17 +45,20 @@ if os.environ.get("READTHEDOCS") == "True":
 
     def run_apidoc(_):
         from sphinx.ext import apidoc
-        apidoc.main([
-            "--force",
-            "--implicit-namespaces",
-            "--module-first",
-            "--separate",
-            "-o",
-            str(PROJECT_ROOT / "docs" / "reference"),
-            str(PACKAGE_ROOT),
-            str(PACKAGE_ROOT / "*.c"),
-            str(PACKAGE_ROOT / "*.so"),
-        ])
+
+        apidoc.main(
+            [
+                "--force",
+                "--implicit-namespaces",
+                "--module-first",
+                "--separate",
+                "-o",
+                str(PROJECT_ROOT / "docs" / "reference"),
+                str(PACKAGE_ROOT),
+                str(PACKAGE_ROOT / "*.c"),
+                str(PACKAGE_ROOT / "*.so"),
+            ]
+        )
 
     def setup(app):
         app.connect("builder-inited", run_apidoc)
